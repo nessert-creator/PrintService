12627da4ecc876a9891ddebd05972f5f46aa29a3
YH <3274501813@qq.com>
2019-10-17 16:31:34 +0800

    ApplayBook

---
+++ PrintServiceServer/QC.MF.Application/KailiApplyBooks/IKailiApplyBookAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using QC.MF.CommonDto;
+using QC.MF.KailiApplyBooks.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QC.MF.KailiApplyBooks
+{
+    public interface IKailiApplyBookAppService : IAsyncCrudAppService<GetListKailiApplyBookDto, int, PagedSortedAndFilteredInputDto, CreateKailiApplyBookDto, UpdateKailiApplyBookDto>
+    {
+    }
+}
